c67cc08ebfcc3bbe9115c3071c780c3f73b6e181
danprince <northwalesdan@gmail.com>
2020-06-02 12:37:12 +0100

    makes sandbox level more interesting

--- src/levels.js
+++ src/levels.js
@@ -2,7 +2,7 @@ import * as Random from "silmarils/rng";
 import { Stage } from "./stage.js";
 
 export function sandbox() {
-  let stage = new Stage(9, 9, 20);
+  let stage = new Stage(20, 20, 20);
 
   // Random pattern in the map tiles
   for (let x = 0; x < stage.width; x++) {
@@ -13,24 +13,33 @@ export function sandbox() {
     }
   }
 
-  stage.spawn("coins", 5, 4);
-  stage.spawn("coins", 1, 1);
-  stage.spawn("coins", 1, 8);
-  stage.spawn("coins", 2, 2);
-  stage.spawn("coins", 8, 6);
+  for (let i = 0; i < 20; i++) {
+    let x = Random.int(0, stage.width);
+    let y = Random.int(0, stage.height);
+    stage.spawn("coins", x, y);
+  }
 
-  stage.spawn("column", 7, 6);
-  stage.spawn("upstairs", 4, 6);
-  stage.spawn("block", 5, 6);
-  stage.spawn("block", 8, 0);
-  stage.spawn("block", 8, 8);
-  stage.spawn("block", 0, 8);
-  stage.spawn("block", 4, 4);
-  stage.spawn("block", 2, 4);
-  stage.spawn("block", 3, 8);
-  stage.spawn("block", 3, 6);
+  for (let i = 0; i < 20; i++) {
+    let x = Random.int(0, stage.width);
+    let y = Random.int(0, stage.height);
+    stage.spawn("column", x, y);
+  }
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 20; i++) {
+    let x = Random.int(0, stage.width);
+    let y = Random.int(0, stage.height);
+    stage.spawn("block", x, y);
+  }
+
+  for (let i = 0; i < 30; i++) {
+    let x = Random.int(0, stage.width);
+    let y = Random.int(0, stage.height);
+    stage.spawn("wall", x, y);
+  }
+
+  stage.spawn("upstairs", 45, 45);
+
+  for (let i = 0; i < 30; i++) {
     let id = Random.item(
       "cyclops", "lamia", "faun", "philosopher", "scholar", "hoplite",
       "hoplite-spearman", "hoplite-captain"
