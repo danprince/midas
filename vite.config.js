f9937103eee39f04c36fe3a4ccd5b38f155a62cc
danprince <northwalesdan@gmail.com>
2020-06-12 18:04:17 +0100

    removes prefresh
    It breaks sourcemaps and it doesn't support custom hooks properly

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-const preactRefresh = require("@prefresh/vite")
-
 module.exports = {
   jsx: "preact",
-  plugins: [preactRefresh()],
 
   // can't use terser because it mangles command function names which
   // breaks Game#dispatch.
