4f921a58eef18cb47b1285da42777cdbf736904e
danprince <northwalesdan@gmail.com>
2020-06-14 11:41:49 +0100

    adds basic medusa ai

--- src/ai.js
+++ src/ai.js
@@ -18,3 +18,28 @@ export function wander(self) {
 
   systems.movement.move(self, direction);
 }
+
+/**
+ * @param {GameObject} self
+ */
+export function medusa(self) {
+  /**
+   * @type {Direction[]}
+   */
+  let directions = [
+    Direction.NORTH,
+    Direction.SOUTH,
+    Direction.EAST,
+    Direction.WEST,
+  ];
+
+  let direction = RNG.element(directions);
+
+  let x = self.x;
+  let y = self.y;
+  let moved = systems.movement.move(self, direction);
+
+  if (moved && RNG.float(0, 1) > 0.8) {
+    game.stage.spawn("snake", x, y);
+  }
+}
